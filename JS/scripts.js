a4bb08737ae2d36823fdd8d75bf55583825bed9d
QuantumGorilla <manotasja@uninorte.edu.co>
2020-07-31 13:43:23 -0500

    NPM scripts

---
+++ JS/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $('#mycarousel').carousel({
+        interval: 2000
+    });
+    $('#carouselButton').click(function() {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        } else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+    /*$('#carousel-play').click(function() {
+    //  $('#mycarousel').carousel('cycle');
+    });*/
+    $('#reserveTButton').click(function() {
+        $('#reserveModal').modal('toggle');
+    });
+    $('.reserveCancelButton').click(function() {
+        $('#reserveModal').modal('hide');
+    });
+    $('#loginLink').click(function() {
+        $('#loginModal').modal('toggle');
+    });
+    $('.loginCancelButton').click(function() {
+        $('#loginModal').modal('hide');
+    });
+});
+
+/*<!--<script> esto es por si quiero usar un tooltip
+        $(document).ready(function() {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>-->*/
